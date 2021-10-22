8b39661dde7209de0b215eb38c782fb667d00bc0
hwx0753 <75604065@qq.com>
2021-10-22 23:23:26 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,20 @@
 import { createStore } from 'vuex'
 
+// vue2.0 创建仓库 new Vuex.Store({})
 export default createStore({
   state: {
+    // 数据
+  },
+  getters: {
+
   },
   mutations: {
+    // 改数据函数
   },
   actions: {
+    // 分模块
   },
   modules: {
+    // vuex的计算属性
   }
 })
