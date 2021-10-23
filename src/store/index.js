689cdd7ab12a8499f15610fbb08b40d36a9bc9ce
hwx0753 <75604065@qq.com>
2021-10-23 23:12:46 +0800

    实现vuex持久化

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,24 @@
 import { createStore } from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+
+import cart from './modules/cart'
+import user from './modules/user'
+import category from './modules/category'
 
 // vue2.0 创建仓库 new Vuex.Store({})
 export default createStore({
-  state: {
-    // 数据
-  },
-  getters: {
-
-  },
-  mutations: {
-    // 改数据函数
-  },
-  actions: {
-    // 分模块
-  },
   modules: {
-    // vuex的计算属性
-  }
+    cart,
+    user,
+    category
+  },
+  //配置插件
+  plugins: [
+    createPersistedState({
+      // 本地存储名字
+      key: 'erabbit-client-pc-124-store',
+      // 指定需要存储的模块
+      paths: ['user', 'cart']
+    })
+  ]
 })
