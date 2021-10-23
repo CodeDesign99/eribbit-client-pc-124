6e1703ad0161dd949849e320c90101a1c15655a2
hwx0753 <75604065@qq.com>
2021-10-24 00:32:37 +0800

    路由设计 - 路由与组件

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,4 @@
 <template>
-  <!-- <router-view /> -->
-  <div class="container">
-    App
-    <button @click="fn">
-      request
-    </button>
-  </div>
+  <!-- 一级路由 -->
+  <RouterView />
 </template>
-<script>
-import request from "@/utils/request";
-export default {
-  name: "App",
-  setup() {
-    const fn = () => {
-      request("member/profile", "get", { a: 10 });
-    };
-    return { fn };
-  }
-};
-</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-// import Home from '../views/Home.vue'
 
-const routes = []
+const Layout = () => import('@/views/Layout')
+const Home = () => import('@/views/home')
+
+const routes = [
+  //一级路由布局容器
+  {
+    path: '/',
+    component: Layout,
+    children: [
+      {
+        path: '/',
+        component: Home
+      }
+    ]
+  }
+]
 // vue2.0 new VueRouter({}) 创建路由实例
 // vue3.0 createRouter({})
 const router = createRouter({
