d54b6b5194f60b594dbee5e1186e385045a0ba73
hwx0753 <75604065@qq.com>
2021-11-04 22:07:40 +0800

    顶级类目-分类商品-布局

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -21,6 +21,16 @@
         </ul>
       </div>
       <!-- 各个分类下推荐商品 -->
+      <div class="ref-goods">
+        <div class="head">
+          <h3>- 海鲜 -</h3>
+          <p class="tag">温暖柔软，品质之选</p>
+          <XtxMore />
+        </div>
+        <div class="body">
+          <GoodsItem v-for="i in 5" :key="i" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -31,8 +41,9 @@ import { findBanner } from "@/api/home";
 import xtxCarousel from "@/components/library/xtx-carousel.vue";
 import { useStore } from "vuex";
 import { useRoute } from "vue-router";
+import GoodsItem from "./components/goods-item";
 export default {
-  components: { xtxCarousel },
+  components: { xtxCarousel, GoodsItem },
   name: "TopCategory",
   setup() {
     // 轮播图
@@ -94,6 +105,32 @@ export default {
           }
         }
       }
+    }
+  }
+  // 推荐商品
+  .ref-goods {
+    background-color: #fff;
+    margin-top: 20px;
+    position: relative;
+    .head {
+      .xtx-more {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+      }
+      .tag {
+        text-align: center;
+        color: #999;
+        font-size: 20px;
+        position: relative;
+        top: -20px;
+      }
+    }
+    .body {
+      display: flex;
+      justify-content: flex-start;
+      flex-wrap: wrap;
+      padding: 0 65px 30px;
     }
   }
 }
