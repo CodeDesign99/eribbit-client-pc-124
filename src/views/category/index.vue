cfc2a332e50cdfcd373153455ae3bd47fc64bc69
hwx0753 <75604065@qq.com>
2021-11-04 22:44:36 +0800

    面包屑切换动画

--- src/views/category/index.vue
+++ src/views/category/index.vue
@@ -4,7 +4,11 @@
       <!-- 渲染面包屑导航 -->
       <XtxBread>
         <XtxBreadItem to="/">首页</XtxBreadItem>
-        <XtxBreadItem>{{ topCategory.name }}</XtxBreadItem>
+        <Transition name="fade-right" mode="out-in">
+          <XtxBreadItem :key="topCategory.id">{{
+            topCategory.name
+          }}</XtxBreadItem>
+        </Transition>
       </XtxBread>
       <!-- 轮播图 -->
       <XtxCarousel :sliders="sliders" style="height:500px" />
