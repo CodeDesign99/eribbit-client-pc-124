bf833a8958374899ceb1f99e5338724a42f45010
hwx0753 <75604065@qq.com>
2021-11-07 16:41:20 +0800

    复选框组件（vue2.0实现v-model双向绑定）

--- src/views/category/sub.vue
+++ src/views/category/sub.vue
@@ -5,16 +5,24 @@
       <SubBread />
       <!-- 筛选区 -->
       <SubFilter />
+      <!-- 复选框 -->
+      <XtxCheckbox v-model="isAllChecked">全选</XtxCheckbox>{{ isAllChecked }}
     </div>
   </div>
 </template>
 
 <script>
+import { ref } from "vue-demi";
 import SubBread from "./components/sub-bread";
 import SubFilter from "./components/sub-filter";
+// import XtxCheckBox from "@/components/xtx-checkbox";
 export default {
   name: "SubCategory",
-  components: { SubBread, SubFilter }
+  components: { SubBread, SubFilter },
+  setup() {
+    const isAllChecked = ref(true);
+    return { isAllChecked };
+  }
 };
 </script>
 
